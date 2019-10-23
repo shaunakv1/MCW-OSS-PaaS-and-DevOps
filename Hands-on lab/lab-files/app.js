7f4b072f7cac1be88cf47f146e67d3ecbd3cac02
Ubuntu <shaunakv1@HackVM.wa1ptcfcfyvehhgbirfdmzopeg.bx.internal.cloudapp.net>
2019-10-23 17:21:06 +0000

    updates

--- Hands-on lab/lab-files/app.js
+++ Hands-on lab/lab-files/app.js
@@ -13,7 +13,7 @@ var userSession = require('./routes/session');
 
 var app = express();
 
-var databaseUrl = 'mongodb://localhost:27017/best-for-you-organics';
+var databaseUrl = 'mongodb://best-for-you-db-hackcosmos:JZNkGpldjWwXy5Igb8jVxFjXp0uXwu9cepz4bdtdnCECyCmDY3Wj0suglfzeoCCpPcKH3JWxnJoPlRZLYqCqYg==@best-for-you-db-hackcosmos.documents.azure.com:10255/best-for-you-organics?ssl=true&replicaSet=globaldb';
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
